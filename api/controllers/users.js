80e8f0cb26ebc8fadb49a12af092d85a56754e27
Kat <katarzynaa89@gmail.com>
2024-01-29 18:21:59 +0000

    Signup email uniquness

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -1,20 +1,30 @@
 const User = require("../models/user");
 
-const create = (req, res) => {
-  const email = req.body.email;
-  const password = req.body.password;
+const checkEmailUniqueness = async (email) => {
+  const existingUser = await User.findOne({ email });
+  return !existingUser;
+};
 
-  const user = new User({ email, password });
-  user
-    .save()
-    .then((user) => {
-      console.log("User created, id:", user._id.toString());
-      res.status(201).json({ message: "OK" });
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(400).json({ message: "Something went wrong" });
-    });
+const create = async (req, res) => {
+  try {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    const isEmailUnique = await checkEmailUniqueness(email);
+    if (!isEmailUnique) {
+      console.log("Auth Error: Email already exists");
+      return res.status(409).json({ message: "Email already exists" });
+    }
+
+    const user = new User({ email, password });
+    await user.save();
+
+    console.log("User created, id:", user._id.toString());
+    res.status(201).json({ message: "OK" });
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({ message: "Something went wrong" });
+  }
 };
 
 const UsersController = {
