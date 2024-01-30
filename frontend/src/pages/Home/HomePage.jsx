471fb95fd830720e8173c15ea0b8644eb1954d63
Piers Ford <piers.ford@hotmail.co.ukl>
2024-01-30 16:18:43 +0000

    Added conditionals to navbar

--- frontend/src/pages/Home/HomePage.jsx
+++ frontend/src/pages/Home/HomePage.jsx
@@ -1,13 +1,15 @@
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 import "./HomePage.css";
+import Navbar from "../Navbar/Navbar";
 
 export const HomePage = () => {
   return (
     <div className="home">
+      <Navbar />
       <h1>Welcome to Acebook!</h1>
-      <Link to="/signup">Sign Up</Link>
-      <Link to="/login">Log In</Link>
+      {/* <Link to="/signup">Sign Up</Link>
+      <Link to="/login">Log In</Link> */}
     </div>
   );
 };
--- frontend/src/pages/Navbar/Navbar.jsx
+++ frontend/src/pages/Navbar/Navbar.jsx
@@ -1,25 +1,92 @@
 import "./navbar.css"
+// import {generateToken} from"api/lib/token.js"
+
+import { useState } from "react";
+
+// const newToken = generateToken(user_id)
 
 
 const Navbar = () => {
-    return (
-        
-        <nav className="nav">
-            <a className ="navTitle" href ="/">Acebook</a>
-            <ul className="navLinks">
+    const [isLoggedIn, setLoggedIn] = useState(window.localStorage.getItem("token") !== null)
+
+    let content
+    // (console.log(isLoggedIn))
+
+    if (isLoggedIn) {
+        // If user is logged in, show these buttons
+        content = (
+            <>
                 <li>
-                    <a href = "/posts">Home</a>
+                    <a href="/dashboard">Feed</a>
+                </li>
+                <li>
+                    <a href="/profile">My Profile</a>
+                </li>
+                <li>
+                    <button onClick={() => setLoggedIn(false)}>Logout</button>
+                </li>
+            </>
+        );
+
+    } else {
+        // If user is not logged in, show these buttons
+        content = (
+            <>
+                <li>
+                    <a href="/login">Login</a>
+                </li>
+                <li>
+                    <a href="/signup">Sign Up</a>
+                </li>
+            </>
+        );
+    }
+
+    return (
+        <nav className="nav">
+            <ul className="navLinks">
+                {/* <li>
+                    <a href="/">Home</a>
                 </li>
                 <li>
                     <a href="/posts">Posts</a>
                 </li>
                 <li>
-                    {/* will route once we have a profile page */}
-                    <a href="">Profile</a>
-                </li>
+                <a href="/posts">Profile</a>
+                </li> */}
+                {content}
             </ul>
-        </nav> 
-    )
+        </nav>
+    );
 }
 
-export default Navbar
+export default Navbar;
+
+
+
+
+
+
+
+// const Navbar = () => {
+//     return (
+        
+//         <nav className="nav">
+//             <a className ="navTitle" href ="/">Acebook</a>
+//             <ul className="navLinks">
+//                 <li>
+//                     <a href = "/posts">Home</a>
+//                 </li>
+//                 <li>
+//                     <a href="/posts">Posts</a>
+//                 </li>
+//                 <li>
+//                     {/* will route once we have a profile page */}
+//                     <a href="">Profile</a>
+//                 </li>
+//             </ul>
+//         </nav> 
+//     )
+// }
+
+// export default Navbar
