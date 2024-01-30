7b7437820b5a7628a77db6c7f6748e381fb8c1a1
siwhelan <simon@swhelan.dev>
2024-01-30 11:49:11 +0000

    create post implemented, renderedf only when a token is available

--- frontend/src/services/posts.js
+++ frontend/src/services/posts.js
@@ -3,13 +3,33 @@ const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
 export const getPosts = async () => {
   const requestOptions = {
-    method: "GET",
+    method: 'GET',
   };
 
-  const response = await fetch(`${BACKEND_URL}/posts`, requestOptions);
+  const response = await fetch(`${BACKEND_URL}/`, requestOptions);
 
   if (response.status !== 200) {
-    throw new Error("Unable to fetch posts");
+    throw new Error('Unable to fetch posts');
+  }
+
+  const data = await response.json();
+  return data;
+};
+
+export const createPost = async (message, token) => {
+  const requestOptions = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({ message }),
+  };
+
+  const response = await fetch(`${BACKEND_URL}`, requestOptions);
+
+  if (!response.ok) {
+    throw new Error('Unable to create post');
   }
 
   const data = await response.json();
