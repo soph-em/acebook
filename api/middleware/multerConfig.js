f678e257a391226605f86b2e687841997e174630
Kat <katarzynaa89@gmail.com>
2024-01-31 16:01:24 +0000

    Implemeted backend logic

---
+++ api/middleware/multerConfig.js
@@ -0,0 +1,18 @@
+const multer = require('multer');
+
+// Define storage for uploaded files
+const storage = multer.diskStorage({
+    // Set desitnation file
+    destination: function (req, file, cb) {
+    cb(null, 'uploads/'); 
+    },
+    // Specify how the uploaded files should be named
+    filename: function (req, file, cb) {
+    cb(null, Date.now() + '-' + file.originalname);
+    },
+});
+
+// Create the multer instance with the defined storage configuration
+const upload = multer({ storage: storage });
+
+module.exports = upload;
