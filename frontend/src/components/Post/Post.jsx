ba50291d287620a37bf80d46744027f77f7ace9d
“Leah” <lnsimonn@hotmail.com>
2024-01-31 14:38:32 +0000

    implement likes btn to post

--- frontend/src/components/Post/Post.jsx
+++ frontend/src/components/Post/Post.jsx
@@ -6,6 +6,7 @@ const Post = (props) => {
     <article key={props.post._id}>
       <p>{props.post.message}</p>
       <p>Posted on: {formattedDate}</p>
+      <button>Likes</button>
     </article>
   );
 };
