2243cb68f5c473e4642c7b524c7a7851fd8d8f95
Kat <katarzynaa89@gmail.com>
2024-02-01 14:46:23 +0000

    Picture display after posting

--- frontend/src/components/Post/NewPost.jsx
+++ frontend/src/components/Post/NewPost.jsx
@@ -1,22 +1,32 @@
 import { useState } from 'react';
+import UploadWidget from './UploadWidget'
 
 const NewPost = ({ onNewPost }) => {
   // State to hold the current value of the textarea
   const [postMessage, setPostMessage] = useState('');
+  const [image, setImage] = useState(null);
+
+  const handleImageUpload = (imageUrl) => {
+    console.log('Cloudinary Result:', imageUrl);
+  // Check if result.info.secure_url contains the image URL
+    setImage(imageUrl);
+  };
 
   // Function to handle form submission
   const handleSubmit = async (event) => {
     // Prevents the page from refreshing on form submit allowing us
     // to handle the submission process manually within the function.
     event.preventDefault();
-
-    // Check if postMessage is not empty
-    if (postMessage) {
+    console.log(postMessage)
+    console.log(image)
+    // Check if postMessage is not   empty
+    if (postMessage || image) {
       // Call the onNewPost function passed from the parent component with the message
-      onNewPost(postMessage);
+      onNewPost(postMessage, image);
 
       // Reset the textarea to empty string after submission
       setPostMessage('');
+      setImage(null);
     }
   };
 
@@ -27,7 +37,9 @@ const NewPost = ({ onNewPost }) => {
         onChange={(message) => setPostMessage(message.target.value)} // Update state on user input
         placeholder="Enter text here..."
       ></textarea>
+
       <br />
+      <UploadWidget onImageUpload={handleImageUpload} />
       <button type="submit">Post</button>
     </form>
   );
---
+++ frontend/src/components/Post/UploadWidget.jsx
@@ -0,0 +1,41 @@
+import { useEffect, useRef } from "react";
+
+const UploadWidget= ( {onImageUpload} ) => {
+  const cloudinaryRef = useRef();
+  useEffect(() => {
+      cloudinaryRef.current = window.cloudinary;
+
+      // Initialize Cloudinary Widget
+  const widget = cloudinaryRef.current.createUploadWidget(
+      {
+        cloudName: "dzkvzncgr", // Cloudinary cloud name
+        uploadPreset: "acebook", // Cloudinary upload preset
+        multiple: false,
+        sources: ["local", "url", "camera", "facebook", "google_drive"],
+      },
+      (error, result) => {
+        console.log(result);
+        if (!error && result && result.event === "success") {
+          // Handle successful image upload
+          const imageUrl = result.info.secure_url;
+          onImageUpload(imageUrl);
+        }
+      }
+    );
+
+    // Attach event listener to open the widget when the component mounts
+    document.getElementById("upload-button").addEventListener("click", () => {
+      widget.open();
+    });
+  }, [onImageUpload]);
+
+
+return (
+  <div>
+      <button id="upload-button">Upload Image</button>
+  </div>
+  );
+};
+  
+
+export default UploadWidget;
