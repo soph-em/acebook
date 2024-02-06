81d190c3b38a3f674e5cb362fe192511aac65dc4
Sophie Earl <sophie.earl@live.com>
2024-02-06 15:59:38 +0000

    styling started

--- frontend/src/components/Post/NewPost.jsx
+++ frontend/src/components/Post/NewPost.jsx
@@ -30,33 +30,36 @@ const NewPost = ({ onNewPost }) => {
   };
 
   return (
-    <form onSubmit={handleSubmit} data-testid="new-post-form">
-      {/* Display the selected image, if available */}
-      {image && (
-        <img
-          src={image}
-          alt="Selected"
-          style={{ maxWidth: "100%", maxHeight: "400px" }}
-        />
-      )}
-      <textarea
-        value={postMessage}
-        onChange={(message) => setPostMessage(message.target.value)}
-        placeholder="Enter text here..."
-        rows="4"
-        className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-      ></textarea>
+    <div className="block border border-grey-light w-full p-3 rounded mb-4">
+      <form onSubmit={handleSubmit} data-testid="new-post-form">
+        {/* Display the selected image, if available */}
+        {image && (
+          <img
+            src={image}
+            alt="Selected"
+            style={{ maxWidth: "100%", maxHeight: "400px" }}
+          />
+        )}
+        <textarea
+          value={postMessage}
+          onChange={(message) => setPostMessage(message.target.value)}
+          placeholder="Enter text here..."
+          rows="4"
+          className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        ></textarea>
 
-      <br />
-      <UploadWidget onImageUpload={handleImageUpload} />
-      <br />
-      <button
-        className="bg-blue-400 text-white py-1 px-4 rounded-md hover:bg-blue-700"
-        type="submit"
-      >
-        Post
-      </button>
-    </form>
+        <br />
+        <div className="flex flex-row justify-center space-x-2">
+          <UploadWidget onImageUpload={handleImageUpload} />
+          <button
+            className="bg-blue-400 text-white py-1 px-4 rounded-md hover:bg-blue-700"
+            type="submit"
+          >
+            Post
+          </button>
+        </div>
+      </form>
+    </div>
   );
 };
 
--- frontend/src/pages/Signup/SignupPage.jsx
+++ frontend/src/pages/Signup/SignupPage.jsx
@@ -1,16 +1,17 @@
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { signup } from '../../services/authentication';
-import { login } from '../../services/authentication';
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { signup } from "../../services/authentication";
+import { login } from "../../services/authentication";
+import Navbar from "../Navbar/Navbar";
 
 export const SignupPage = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [username, setUsername] = useState('');
-  const [emailError, setEmailError] = useState('');
-  const [passwordError, setPasswordError] = useState('');
-  const [usernameError, setUsernameError] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  const [usernameError, setUsernameError] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
   const navigate = useNavigate();
 
   const handleSubmit = async (event) => {
@@ -18,74 +19,110 @@ export const SignupPage = () => {
 
     try {
       await signup(username, email, password);
-      console.log('logging in...')
+      console.log("logging in...");
       const token = await login(email, password);
       window.localStorage.setItem("token", token);
-      alert('Signup successful.\nYou are now being logged in.')
+      alert("Signup successful.\nYou are now being logged in.");
       navigate("/");
-      console.log('redirecting...');
+      console.log("redirecting...");
       // navigate('/login');
     } catch (err) {
       console.error(err);
-      if (err.message.includes('Email already exists')) {
-        setEmailError('Email already exists. Please use a different email.');
-      } else if (err.message.includes('Username already exists')) {
-        setUsernameError('Username already exists. Please use a different username.');
-      } else if (err.message.includes('Password does not meet requirements')){
-        setPasswordError('Password must be 8 characters or longer.')
-      }else {
-        setErrorMessage('An error occurred during signup. Please try again.');
+      if (err.message.includes("Email already exists")) {
+        setEmailError("Email already exists. Please use a different email.");
+      } else if (err.message.includes("Username already exists")) {
+        setUsernameError(
+          "Username already exists. Please use a different username."
+        );
+      } else if (err.message.includes("Password does not meet requirements")) {
+        setPasswordError("Password must be 8 characters or longer.");
+      } else {
+        setErrorMessage("An error occurred during signup. Please try again.");
       }
     }
   };
 
   const handleEmailChange = (event) => {
     setEmail(event.target.value);
-    setEmailError('');
+    setEmailError("");
   };
 
   const handlePasswordChange = (event) => {
     setPassword(event.target.value);
-    setPasswordError('');
+    setPasswordError("");
   };
 
   const handleUsernameChange = (event) => {
     setUsername(event.target.value);
-    setUsernameError('');
+    setUsernameError("");
   };
 
   return (
     <>
-      <h2>Signup</h2>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="username">Username:</label>
-        <input
-          id="username"
-          type="text"
-          value={username}
-          onChange={handleUsernameChange}
-        />
-        {usernameError && <p>{usernameError}</p>}
-        <label htmlFor="email">Email:</label>
-        <input
-          id="email"
-          type="email"
-          value={email}
-          onChange={handleEmailChange}
-        />
-        {emailError && <p>{emailError}</p>}
-        <label htmlFor="password">Password:</label>
-        <input
-          placeholder="Password"
-          id="password"
-          type="password"
-          value={password}
-          onChange={handlePasswordChange}
-        />
-        {passwordError && <p>{passwordError}</p>}
-        <input role="submit-button" id="submit" type="submit" value="Submit" />
-        {errorMessage && <p>{errorMessage}</p>}
-      </form>
+      <div className="bg-grey-lighter min-h-screen flex flex-col">
+        <Navbar />
+        <div className="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
+          <div className="bg-white px-6 py-8 rounded shadow-md text-black w-full">
+            <h2 className="mb-8 text-3xl text-center">Sign up for Acebook</h2>
+            <form onSubmit={handleSubmit}>
+              <label htmlFor="username">Username:</label>
+              <input
+                placeholder="Username"
+                id="username"
+                type="text"
+                value={username}
+                onChange={handleUsernameChange}
+                className="block border border-grey-light w-full p-3 rounded mb-4"
+              />
+              {usernameError && <p>{usernameError}</p>}
+              <label htmlFor="email">Email:</label>
+              <input
+                placeholder="Email"
+                id="email"
+                type="email"
+                value={email}
+                onChange={handleEmailChange}
+                className="block border border-grey-light w-full p-3 rounded mb-4"
+              />
+              {emailError && <p>{emailError}</p>}
+              <label htmlFor="password">Password:</label>
+              <input
+                placeholder="Password"
+                id="password"
+                type="password"
+                value={password}
+                onChange={handlePasswordChange}
+                className="block border border-grey-light w-full p-3 rounded mb-4"
+              />
+              {passwordError && <p>{passwordError}</p>}
+              <input
+                role="submit-button"
+                id="submit"
+                type="submit"
+                value="Submit"
+                className="w-full text-center py-3 rounded bg-slate-700 text-white hover:bg-green-dark focus:outline-none my-1"
+              />
+              {errorMessage && <p>{errorMessage}</p>}
+              <div className="text-center text-sm text-grey-dark mt-4">
+                By signing up, you agree to the
+                <a
+                  className="no-underline border-b border-grey-dark text-grey-dark"
+                  href="#"
+                >
+                  Terms of Service
+                </a>{" "}
+                and
+                <a
+                  className="no-underline border-b border-grey-dark text-grey-dark"
+                  href="#"
+                >
+                  Privacy Policy
+                </a>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
