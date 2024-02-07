197b9e465b977dee0a1e51f4c51002d4dc1271a3
siwhelan <simon@swhelan.dev>
2024-02-07 09:51:38 +0000

    Fixed conflicts

--- frontend/src/components/Post/Post.jsx
+++ frontend/src/components/Post/Post.jsx
@@ -1,18 +1,35 @@
+import DeleteButton from "./DeleteButton";
+import EditPostForm from "./EditPostForm";
 import Comments from "../Comments/Comment";
 import LikeButton from "../Likes/LikeButton";
 import LikeCounter from "../Likes/LikeCounter";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom"; // Import Link from react-router-dom
+import { getUser } from "../../services/users";
 
 const Post = (props) => {
   const [likes, setLikes] = useState(props.post.likes);
+  const [username, setUsername] = useState("");
+  const [pfp, setPfp] = useState(null);
+  const [isEditing, setIsEditing] = useState(false);
+  const postUsername = props.post.createdBy.username;
   const formattedDate = new Date(props.post.createdAt).toLocaleString("en-GB");
-  const username = props.post.createdBy.username;
-  const userId = props.post.createdBy._id; // Assuming the user ID is available here
+  const userId = props.post.createdBy._id;
   const token = props.token; // Token passed as a prop
   const allowComments = props.allowComments;
 
-  // console.log("Image URL:", props.post.image);
+  useEffect(() => {
+    getUser().then((data) => {
+      setUsername(data.username);
+      setPfp(data.image);
+    });
+  }, []);
+
+  const handleUpdate = (updatedPost) => {
+    console.log("Post updated successfully");
+    setIsEditing(false); // Switch back to view mode after updating
+    props.onUpdatePost(updatedPost);
+  };
 
   return (
     <article
@@ -25,33 +42,50 @@ const Post = (props) => {
           <div className="w-full flex justify-center">
             <img
               src={props.post.image}
-              alt={`Posted by ${username}`}
+              alt={`Posted by ${postUsername}`}
               className="max-w-full max-h-96 object-cover"
             />
           </div>
         )}
-        <div className="text-sm">
+        <div className="text-sm flex justify-center items-center">
           Posted by:{" "}
           <Link to={`/profile/${userId}`} className="text-blue-500">
-            {" "}
-            {/* Use Link component for routing */}
             {username}
           </Link>
+          <img className="h-5 ml-2" src={pfp} alt="Profile" />
         </div>
         <div className="text-xs text-gray-400">Posted on: {formattedDate}</div>
       </div>
-      <Comments
-        postId={props.post._id}
-        token={token}
-        username={username}
-        allowComments={allowComments}
-      />
-      <LikeButton
-        postId={props.post._id}
-        postLikes={likes}
-        setLikes={setLikes}
-      />
-      <LikeCounter likes={likes} />
+
+      {isEditing ? (
+        <EditPostForm
+          postId={props.post._id}
+          initialMessage={props.post.message}
+          onUpdate={handleUpdate}
+        />
+      ) : (
+        <>
+          <Comments
+            postId={props.post._id}
+            token={token}
+            username={username}
+            allowComments={allowComments}
+          />
+          <LikeButton
+            postId={props.post._id}
+            postLikes={likes}
+            setLikes={setLikes}
+          />
+          <LikeCounter likes={likes} />
+          <div>
+            <button onClick={() => setIsEditing(true)}>Edit</button>
+            <DeleteButton
+              postId={props.post._id}
+              onDeletePost={props.onDeletePost}
+            />
+          </div>
+        </>
+      )}
     </article>
   );
 };
