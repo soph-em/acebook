3ba596a9688bd2509f9bd570f4df3b80a9420fd9
Sophie Earl <sophie.earl@live.com>
2024-02-08 15:18:03 +0000

    app.js fixes

--- api/app.js
+++ api/app.js
@@ -6,49 +6,42 @@ const usersRouter = require("./routes/users");
 const postsRouter = require("./routes/posts");
 const authenticationRouter = require("./routes/authentication");
 const commentsRouter = require("./routes/comments");
-// const tokenChecker = require("./middleware/tokenChecker");
 
 const app = express();
 
-// Allow requests from any client
-// docs: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
-// docs: https://expressjs.com/en/resources/middleware/cors.html
 app.use(cors());
-
-//render/deployment handling
-if (process.env.NODE_ENV === "production") {
-  //*Set static folder up in production
-  app.use(express.static("../frontend/dist"));
-
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "../frontend/dist", "index.html"))
-  );
-}
-
-// Parse JSON request bodies, made available on `req.body`
 app.use(bodyParser.json());
 
 // API Routes
-app.use("/", commentsRouter);
-app.use("/users", usersRouter);
-app.use("/", postsRouter);
-app.use("/tokens", authenticationRouter);
-app.use("/like/:id", postsRouter);
+app.use("/api/comments", commentsRouter); // Fixed the route prefix here
+app.use("/api/users", usersRouter);
+app.use("/api/posts", postsRouter);
+app.use("/api/tokens", authenticationRouter);
 
-// 404 Handler
+// Static files and SPA fallback in production
+if (process.env.NODE_ENV === "production") {
+  console.log(
+    "Setting up static files from:",
+    path.resolve(__dirname, "../frontend/dist")
+  );
+  app.use(express.static(path.resolve(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    const indexPath = path.resolve(__dirname, "../frontend/dist/index.html");
+    console.log("Attempting to send file from:", indexPath);
+    res.sendFile(indexPath);
+  });
+}
+
+// 404 Handler should be after API and static routes
 app.use((_req, res) => {
   res.status(404).json({ err: "Error 404: Not Found" });
 });
 
-// Error handler
+// General error handler
 app.use((err, _req, res, _next) => {
   console.error(err);
-
-  if (process.env.NODE_ENV === "development") {
-    res.status(500).send(err.message);
-  } else {
-    res.status(500).json({ err: "Something went wrong" });
-  }
+  res.status(500).json({ err: "Something went wrong" });
 });
 
 module.exports = app;
