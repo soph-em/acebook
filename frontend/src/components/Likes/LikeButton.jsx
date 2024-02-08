8a796ef83f3151d89c596cc6452f22c4a5dc361d
“Leah” <lnsimonn@hotmail.com>
2024-02-08 12:04:49 +0000

    implement frontend functionality only like one post

--- frontend/src/components/Likes/LikeButton.jsx
+++ frontend/src/components/Likes/LikeButton.jsx
@@ -6,19 +6,38 @@ const LikeButton = ({postId, postLikes, setLikes}) => {
     const [isLiked, setIsLiked] = useState(false); //Default False
     
     const handleLikeClick = async () => {
-      //FETCH - PUT request
-    const response = await fetch(`${BACKEND_URL}/like/${postId}`, {
-      method: "PUT", 
-      headers: {
+      if (isLiked) return; //If the post is already liked do nothing.
+
+      try {
+        //Fetch Request
+        const response = await fetch(`${BACKEND_URL}/like/${postId}`, {
+        method: "PUT", 
+        headers: {
         "Content-Type": "application/json",
         "Authorization": `Bearer ${window.localStorage.getItem("token")}`,
       },
     });
-    const data  = await response.json()
+    //If request is succesful..
+    //updates status to show post as been liked
+    //add postID to the likes array and updates frontend
+    if (response.ok) {
+      
+      setIsLiked(true);
+      setLikes([...postLikes, postId]) 
+    } else {
+    //If request fails
+      const errorData = await response.json();
+      console.error('Unable to like post', errorData.msg)
+    }
+  } catch (error) {
+    console.error('Error liking the post', error.message)
+  }
+};
+    /*const data  = await response.json()
     setLikes([...postLikes, data.user_id])
     console.log(postLikes)
     
-  }
+  }*/
 
   return (
     <button onClick={handleLikeClick} disabled={isLiked}>
